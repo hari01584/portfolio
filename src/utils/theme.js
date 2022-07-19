c3a54f2200ac1263f1a3fd19a0a54040e56f0760
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2022-07-19 23:28:38 +0530

    portfolio!

---
+++ src/utils/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    blue: "#3371FF",
+    white: "#FFFFFF",
+    black: "#1A202C",
+  },
+  devices: {
+    tablet: "(max-width: 991.98px)",
+  },
+}
+
+export default theme
